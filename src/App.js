adcb826b6a713797ac24707d11a5e3c1f366da3c
shubhampant13 <shubhampant13@gmail.com>
2018-08-22 17:44:38 +0530

    Use of clarifai api to detect the start and end coordinates of the human face in the image

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import ImageLinkForm from './Component/ImageLinkForm/ImageLinkForm.js';
 import Rank from './Component/Rank/Rank.js';
 import Particles from 'react-particles-js';
 import './App.css';
+import Clarifai from 'clarifai';
+import FaceRecognition from './Component/FaceRecognition/FaceRecognition.js';
+
+// initialize with your api key. 
+const app = new Clarifai.App({
+ apiKey: 'e2f4e76400884ce29d2f3417ab199a3b'
+});
+
 
 const particlesOptions = {
                 particles: {
@@ -19,6 +27,30 @@ const particlesOptions = {
               }
 
 class App extends Component {
+  constructor() {
+     super();
+     this.state = {
+       input: '',
+       imageUrl : '',
+     }
+  }
+
+onInputChange = (event) => {
+  this.setState({input: event.target.value});
+}
+
+onButtonSubmit = () => {
+  this.setState({imageUrl: this.state.input});
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+}
+
   render() {
     return (
       <div className="App">
@@ -28,12 +60,12 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-         { /* 
-           
-           <FaceRecognition />
-           */
-         }  
+        <ImageLinkForm 
+            onInputChange={this.onInputChange} 
+            onButtonSubmit={this.onButtonSubmit} 
+        /> 
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+             
       </div>
     );
   }
---
+++ src/Component/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const FaceRecognition = ({imageUrl}) => {
+   return (
+   		<div className='center ma'>
+   		  <div className='absolute mt2'>
+   		   <img src= {imageUrl} width='500px' height='auto'
+   		    alt="Image" />
+   		  </div>  
+   		</div>
+   	);
+}
+
+export default FaceRecognition;
