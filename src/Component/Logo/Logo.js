8b5d124b9a86361734673b1e0577f0e3e8fb50e8
shubhampant13 <shubhampant13@gmail.com>
2018-08-24 15:07:14 +0530

    Use of clarifai face_detect_model API , creation of rect. box on the face

--- src/Component/Logo/Logo.js
+++ src/Component/Logo/Logo.js
@@ -8,7 +8,7 @@ const Logo = () => {
    		<div className='ma4 mt0'>
           <Tilt className="Tilt br2 shadow-2" options={{ max : 45 }} style={{ height: 160, width: 150 }} >
              <div className="Tilt-inner pa3">
-              <img style={{paddingTop:'3px'}} src={Brain} alt="Brain image" />
+              <img style={{paddingTop:'3px'}} src={Brain} alt='can"t be displayed' />
              </div>
           </Tilt>
    		</div>
